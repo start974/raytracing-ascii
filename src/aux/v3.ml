834fda3daf810a7f252b3e034ef76520124c3a85
Emile Trotignon <emile.trotignon@gmail.com>
2021-10-29 13:38:20 +0200

    geometry colinear

--- src/aux/v3.ml
+++ src/aux/v3.ml
@@ -5,3 +5,6 @@ let is_close v1 v2 =
   IFloat.is_close (x v1) (x v2)
   && IFloat.is_close (y v1) (y v2)
   && IFloat.is_close (z v1) (z v2)
+
+let is_colinear v1 v2 =
+  is_close (unit v1) (unit v2) || is_close (smul (-1.) (unit v1)) (unit v2)
--- src/aux/v3.mli
+++ src/aux/v3.mli
@@ -1,3 +1,5 @@
 include module type of Gg.V3
 
 val is_close : t -> t -> bool
+
+val is_colinear : t -> t -> bool
